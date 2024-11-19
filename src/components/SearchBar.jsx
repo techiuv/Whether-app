51da60fbd2f449fa74cb24d2c86bb279cb7831f3
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-11-19 19:58:40 +0530

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SearchBar = () => {
     return (
         <div className='w-[80vw] h-12 md:w-[400px] flex justify-center items-center gap-1 md:gap-2 mx-auto'>
-            <input type="search" name="search" placeholder='Search city' className='cursor-text w-[80%] h-[100%] px-1 md:px-3 py-1 bg-transparent border-none outline-none focus:outline-none focus:bg-transparent text-white text-sm md:text-lg font-normal placeholder:text-slate-300 backdrop-blur-md search-no-clear' autoComplete='off' />
+            <input type="search" name="search" placeholder='Search city' className='cursor-text w-[80%] h-[100%] px-1 md:px-3 py-1 bg-transparent border-none outline-none focus:outline-none focus:bg-transparent text-white text-sm md:text-lg font-normal placeholder:text-slate-300 backdrop-blur-md search-no-clear' autoComplete='off' aria-label="Search for weather" pattern="[A-Za-z\s]+" spellcheck="false" minlength="2" maxlength="50" />
 
             <button className='cursor-pointer bg-transparent '>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" class="bi bi-search" viewBox="0 0 16 16">
