4c71c3e851faa7a20fa2a9a45088a08b509d41fe
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-11-22 21:39:24 +0530

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import SeekBar from './seekbar';
+import SeekBar from './Seekbar';
 import SunArc from './SunArc';
 import WeatherMetrics from './WatherMetrics';
 import SkeletonLoader from './SkeletonLoader';
