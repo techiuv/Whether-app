d1a472c8df9d2fdb5268112d93c02af04328b399
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-11-22 21:04:52 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
-    "./src/**/*.{html,js}",
-    "./public/index.html"
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -12,22 +13,6 @@ module.exports = {
         sunGlow: '#fdeea2',      
         textPrimary: '#ffffff',  
         textSecondary: '#cce6ff',
-        
-        // Individual colors from the extracted palettes
-        sunrise1: "#FDE6A0",
-        sunrise2: "#F7A676",
-        sunrise3: "#F16745",
-        sunset1: "#E26D5A",
-        sunset2: "#D44177",
-        sunset3: "#9656A1",
-        night1: "#031D44",
-        night2: "#033860",
-        night3: "#36536B",
-      },
-      backgroundImage: {
-        'sunrise-gradient': 'linear-gradient(90deg, #FDE6A0, #F7A676, #F16745)',
-        'sunset-gradient': 'linear-gradient(90deg, #E26D5A, #D44177, #9656A1)',
-        'night-gradient': 'linear-gradient(90deg, #031D44, #033860, #36536B)',
       },
     },
   },
