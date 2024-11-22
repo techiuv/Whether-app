0afedce3dc22aa26b2da8bcefcb7b8f275302f55
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-11-22 14:48:11 +0530

    Update LoadingSpinner.jsx

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,1 +1,22 @@
+import React from 'react';
+import './LoadingSpinner.css'; 
+
+const LoadingSpinner = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen w-full">
+      <div className="lds-roller relative inline-block h-16 w-16">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
 
