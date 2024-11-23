c262e3c4542df74a14a5e4ef8241e328fa941a0c
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-11-24 00:07:04 +0530

    Create WeatherMap.jsx

---
+++ src/components/WeatherMap.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const WeatherMap = () => {
+  return (
+    <div style={{ height: "500px", width: "100%" }}>
+      <MapContainer center={[20.5937, 78.9629]} zoom={5} scrollWheelZoom={false}>
+        {/* TileLayer for OpenStreetMap or another map source */}
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {/* Add your layers like weather overlays here */}
+      </MapContainer>
+    </div>
+  );
+};
+
+export default WeatherMap;
