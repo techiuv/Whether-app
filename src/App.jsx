c8c4d79fcb9e4e7edc1f7da9888a3beca519185b
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-11-23 18:57:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,37 +8,41 @@ function App() {
 
   // Function to fetch city name based on user's geolocation
   const fetchCityFromGeolocation = async () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        async (position) => {
-          const { latitude, longitude } = position.coords;
-
-          const apiKey = import.meta.env.VITE_OPEN_WEATHER_API;
-          const apiUrl = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${apiKey}`;
-
-          try {
-            const response = await fetch(apiUrl);
-            if (response.ok) {
-              const data = await response.json();
-              if (data.length > 0) {
-                const detectedCity = data[0].name;
-                setCity(detectedCity);
-              }
-            } else {
-              console.error('Error fetching city data:', response.statusText);
-            }
-          } catch (error) {
-            console.error('Error:', error);
-          }
-        },
-        (error) => {
-          console.error('Geolocation error:', error);
-          alert('Geolocation error:', error);
-        }
-      );
-    } else {
+    if (!navigator.geolocation) {
       alert('Geolocation is not supported by this browser.');
+      return;
     }
+
+    navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        const { latitude, longitude } = position.coords;
+
+        const apiKey = import.meta.env.VITE_OPEN_WEATHER_API;
+        const apiUrl = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${apiKey}`;
+
+        try {
+          const response = await fetch(apiUrl);
+          if (response.ok) {
+            const data = await response.json();
+            if (data.length > 0) {
+              const detectedCity = data[0].name;
+              setCity(detectedCity);
+            } else {
+              alert('Could not determine the city from your location.');
+            }
+          } else {
+            console.error('Error fetching city data:', response.statusText);
+          }
+        } catch (error) {
+          console.error('Error:', error);
+          alert('Failed to fetch city data. Please try again later.');
+        }
+      },
+      (error) => {
+        console.error('Geolocation error:', error);
+        alert('Geolocation error: ' + error.message);
+      }
+    );
   };
 
   useEffect(() => {
