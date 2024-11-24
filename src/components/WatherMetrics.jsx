119690e885c231913158cc45e044e1b4d8a04da3
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-11-24 17:32:15 +0530

    Update WatherMetrics.jsx

--- src/components/WatherMetrics.jsx
+++ src/components/WatherMetrics.jsx
@@ -25,7 +25,7 @@ const WeatherMetrics = ({ humidity, pressure, wind, visibility }) => {
             <div className="rounded-2xl bg-secondary p-4 text-textSecondary">
                 <p className="text-sm font-normal flex items-center gap-1">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-[1rem] w-[1rem] md:h-[1rem] md:w-[1rem]" fill="currentColor" viewBox="0 0 16 16">
-                        <path d="M12.5 2A2.5 2.5 0 0 0 10 4.5a.5.5 0 0 1-1 0A3.5 3.5 0 1 1 12.5 8H.5a.5.5 0 0 1 0-1h12a2.5 2.5 0 0 0 0-5m-7 1a1 1 0 0 0-1 1 .5.5 0 0 1-1 0 2 2 0 1 1 2 2h-5a.5.5 0 0 1 0-1h5a1 1 0 0 0 0-2M0 9.5A.5.5 0 0 1 .5 9h10.042a3 3 0 1 1-3 3 .5.5 0 0 1 1 0 2 2 0 1 0 2-2H.5a.5.5 0 0 1-.5-.5" />
+                        <path d="M8 2a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 6.095 0 7.555 0 9.318 0 11.366 1.708 13 3.781 13h8.906C14.502 13 16 11.57 16 9.773c0-1.636-1.242-2.969-2.834-3.194C12.923 3.999 10.69 2 8 2m2.354 5.146a.5.5 0 0 1-.708.708L8.5 6.707V10.5a.5.5 0 0 1-1 0V6.707L6.354 7.854a.5.5 0 1 1-.708-.708l2-2a.5.5 0 0 1 .708 0z" />
                     </svg>Pressure
                 </p>
                 <p className="text-lg text-white font-medium">{pressure} mb</p>
